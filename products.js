3f05bf7e5b14c7f4ae8cbf81b9cd8053d6898381
authormexin <authormexin@gmail.com>
2026-04-15 22:57:42 +0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,6 @@
+const products = [
+  { id: 1, name: "Travel Bag", price: 1200 },
+  { id: 2, name: "School Backpack", price: 900 },
+  { id: 3, name: "Laptop Bag", price: 1500 },
+  { id: 4, name: "Hand Bag", price: 800 }
+];
